edb57c0851393c8cc596fcd96692f9ccbe880cae
Pavel <88939124+BugakovPavel@users.noreply.github.com>
2021-12-12 02:16:49 +0300

    Add files via upload

---
+++ FirstProject/FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Павел Бугаков on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
